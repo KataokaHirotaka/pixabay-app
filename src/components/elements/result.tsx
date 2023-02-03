c3a7ce147dbef0633380777aa28ef536c36deafa
HirotakaKataoka <h-kataoka@bfp.co.jp>
2023-02-03 19:39:59 +0900

    ローディング画面の処理など

--- src/components/elements/result.tsx
+++ src/components/elements/result.tsx
@@ -1,40 +1,72 @@
 /* eslint-disable react/jsx-key */
-import { Center, Container, Grid, GridItem, Heading } from '@chakra-ui/react'
+import { Center, Container, Flex, Heading, Box } from '@chakra-ui/react'
+import Link from 'next/link'
 import { Key } from 'react'
+import { Url } from 'url'
+import { usePixabayDataSWR } from '~/src/hooks/usePixabayDataSWR'
 import { resultProps } from '~/src/types'
 
-export const Result = ({ imgData }: resultProps) => {
-  console.log(imgData, 'asfdjioasdfjio')
+export const Result = ({ url }: resultProps) => {
+  const { data, isLoading, isError } = usePixabayDataSWR(url)
 
-  if (imgData) {
+  if (url != '' && isLoading) {
     return (
-      <Container>
-        <Center>
-          <Heading>検索結果</Heading>
-        </Center>
-        <Center>
-          <Grid>
-            {imgData.map(
-              (item: {
-                id: Key | null | undefined
-                largeImgURL: string | undefined
-              }) => {
-                const id = item.id
-                const imgUrl = item.largeImageURL
-                return (
-                  <GridItem>
-                    <div key={id}>
-                      <img src={imgUrl}></img>
-                    </div>
-                  </GridItem>
-                )
-              }
-            )}
-          </Grid>
-        </Center>
+      <Center mt="40px">
+        <Heading as="h2">ローディング中</Heading>
+      </Center>
+    )
+  }
+
+  if (isError) {
+    return (
+      <Center mt="40px">
+        <Heading as="h2">エラー</Heading>
+      </Center>
+    )
+  }
+
+  if (data.total === 0) {
+    return (
+      <Center mt="40px">
+        <Heading as="h2">
+          残念ながら該当するものがみつかりませんでした。
+        </Heading>
+      </Center>
+    )
+  }
+
+  if (data) {
+    return (
+      <Container mt="20px" maxW="1200px">
+        <Flex flexWrap="wrap" justifyContent="space-between" mt="40px">
+          {data.hits.map(
+            (item: {
+              pageURL: Url
+              webformatWidth: number
+              id: Key | null | undefined
+              largeImageURL: string | undefined
+            }) => {
+              return (
+                <Box
+                  key={item.id}
+                  flexBasis={item.webformatWidth / 2 + 'px'}
+                  flexGrow="1"
+                  flexShrink="1"
+                  height="70%"
+                  ml="10px"
+                  mb="10px"
+                >
+                  <Link href={item.pageURL}>
+                    <img src={item.largeImageURL}></img>
+                  </Link>
+                </Box>
+              )
+            }
+          )}
+        </Flex>
       </Container>
     )
-  } else {
-    return <Center>{/* <Heading>検索結果</Heading> */}</Center>
   }
+
+  return <Center mt="40px"></Center>
 }
